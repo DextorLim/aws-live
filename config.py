9fcdf827fa7238c5d688fc56bee93b1aba3c4e31
DextorLim <154388701+DextorLim@users.noreply.github.com>
2023-12-20 08:38:33 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-1.cxqky3cuk5ok.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "limriying-employee"
+customregion = "us-east-1"
+
